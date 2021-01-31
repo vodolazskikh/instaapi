9fb54eda59f33aa795632a4b96cf56f5cf3d482f
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-31 14:34:58 +0700

    get auth

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const config = {
-  instagramApi: "http://api.instagram.com/",
+  instagramApi: "https://api.instagram.com/",
   appId: "476292603759149",
-  appKey: "f612bebf6feeff0d08d945433cfcce61",
+  appKey: process.env.API_KEY,
   api: "https://instaapigram.herokuapp.com/",
+  clientApp: "https://insta-client-test.vercel.app/",
 };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
+import fetch from "node-fetch";
+import { config } from "./config";
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,8 +16,32 @@ app
     res.send("Апи работает окей - на ts");
   })
   .get("/auth", (req: Request, res: Response) => {
-    const code = req.query.code;
-    res.send(code);
+    const code = req.query.code as string;
+    const FormData = require("form-data");
+
+    const form = new FormData();
+    form.append("code", code);
+    form.append("client_id", config.appId);
+    form.append("client_secret", config.appKey);
+    form.append("grant_type", "authorization_code");
+    form.append("redirect_uri", `${config.api}auth`);
+
+    const options = {
+      method: "POST",
+      body: form,
+      headers: form.getHeaders(),
+    };
+    if (!code) {
+      res.send("кек");
+    }
+
+    fetch(`${config.instagramApi}oauth/access_token`, options)
+      .then((res) => res.json())
+      .then((json) => {
+        res.redirect(
+          `${config.clientApp}token?accessToken=${json.access_token}&userId=${json.user_id}`
+        );
+      });
   })
 
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
